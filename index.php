df4a406ff86de6b5d74f8de6f0e70dab7153c750
marabesi <matheus.marabesi@gmail.com>
2016-12-20 21:43:07 -0200

    added simple communication with firebase

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-<h1>Hello world</h1>
+<?php
+
+require 'vendor/autoload.php';
+
+$env = new \Dotenv\Dotenv('.');
+
+$env->load();
+
+$url = getenv('DEFAULT_URL');
+
+$token = getenv('DEFAULT_TOKEN');
+
+$firebase = new \Firebase\FirebaseLib($url, $token);
+
+header('Content-Type: application/json');
+
+if (array_key_exists('temperature', $_GET)) {
+    $firebase->set('/', [
+        'dht11' => [
+            'temperature' => $_GET['temperature']
+        ]
+    ]);
+}
+
+print $firebase->get('/');
